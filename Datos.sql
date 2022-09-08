c20beafd60ac9ae85ac7311f61c01f2afc1e5a0f
TobarCajasFabricio <fabriciotobar@utelvt.edu.ec>
2022-09-08 14:19:25 -0500

    Base de Datos

---
+++ Datos.sql
@@ -0,0 +1,3 @@
+create database MiPlataforma;
+use MiPlataforma;
+create table persona(idpersona int, nombre varchar(20) apellido varchar (20), cedula varchar (10), idgenero int, fechadenacimiento date, idnacionalidad int, idetnia int, idestadocivil int);
